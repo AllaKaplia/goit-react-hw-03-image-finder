3532e75fc64a4808d7733fb71008865a548b5d27
Alla Kaplia <kaplia.alla18@gmail.com>
2023-06-18 14:34:14 +0300

    Create App.jsx

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Container } from './App.styled';
+
+class App extends Component {
+  render () {
+    return (
+      <Container>
+        React homework template
+      </Container>
+    );
+  }
+};
+
+export default App;
