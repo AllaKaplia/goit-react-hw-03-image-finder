dda89fd12d3935ac9751d119c0c9974262c53ec3
Alla Kaplia <kaplia.alla18@gmail.com>
2023-06-19 03:12:31 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import SearchBar from "components/SearchBar";
 import { Container } from './App.styled';
+import ImageGallery from "components/ImageGallery";
 
 class App extends Component {
+  state = {
+    imageName: '',
+  }
+
+  handleSearchNameSubmit = (imageName) => {
+    this.setState({ imageName });
+  }
+
   render () {
     return (
       <Container>
-        React homework template
+        <SearchBar onSubmit={this.handleSearchNameSubmit} />
+        <ImageGallery imagesName={this.state.imageName}/>
+        <ToastContainer autoClose={3000} theme="dark" />
       </Container>
     );
   }
