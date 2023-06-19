b7af1024aa761628a182316c9f08dd8e3f44118b
Alla Kaplia <kaplia.alla18@gmail.com>
2023-06-19 03:12:41 +0300

    Create Button.jsx

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,8 @@
+import { ButtonLoad } from "./Button.styled";
+
+
+const Button = ({ onClick }) => {
+    return <ButtonLoad type="button" onClick={onClick} >Load more</ButtonLoad>
+}
+
+export default Button;
